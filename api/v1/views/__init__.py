c3e17e219b5fb16d0914b71823cc11721ffbcf0d
amineirb <contact@irbaine.com>
2024-04-27 15:18:26 -0700

    Done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """
-    setup the views for v1 api
+views
 """
 
-from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint('/api/v1/app_views', __name__)
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.users import *
+from api.v1.views.places_amenities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,46 @@
 #!/usr/bin/python3
 """
-    index file for v1 api
+index
 """
 
+from flask import jsonify
 from api.v1.views import app_views
 
+from models import storage
 
-@app_views.route('/status')
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """Return a JSON of the status"""
-    return {"status": "OK"}
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
